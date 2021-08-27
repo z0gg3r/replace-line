8d4a012ad8cafcf8fe0dc6381f95db24c08d348d
z0gg3r <z0gg3r@users.no-reply.github.com>
2021-08-27 17:05:36 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+// SPDX-FileCopyrightText: 2021 zocker <zockerfreunde03.info@gmx.de>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use getopt::Opt;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let mut opts = getopt::Parser::new(&args, "f:r:h");
+
+    let mut file = String::new();
+    let mut pattern = String::new();
+    let mut help = false;
+
+    loop {
+        match opts
+            .next()
+            .transpose()
+            .expect("ERROR 3: Could not parse arguments!")
+        {
+            None => break,
+            Some(opt) => match opt {
+                Opt('f', Some(arg)) => file = arg.clone(),
+                Opt('r', Some(arg)) => pattern = arg.clone(),
+                Opt('h', None) => help = true,
+                _ => unreachable!(),
+            },
+        }
+    }
+
+    if help || pattern.is_empty() {
+        print_usage(&args[0]);
+    } else if file.is_empty() {
+        let mut v: Vec<String> = vec![];
+
+        loop {
+            let mut input = String::new();
+
+            let bytes = std::io::stdin()
+                .read_line(&mut input)
+                .expect("ERROR 4: Could not read from stdin!");
+            input = input.trim().to_string();
+
+            if bytes == 0 {
+                break;
+            }
+
+            if !input.contains(&pattern) {
+                v.push(input);
+            }
+        }
+
+        for line in v {
+            println!("{}", line);
+        }
+    } else {
+        let input = read_file(file.clone());
+        let input = if input.is_ok() {
+            input.unwrap()
+        } else {
+            String::new()
+        };
+        if !input.is_empty() {
+            let v = replace(split(input), pattern.clone());
+
+            for line in v {
+                println!("{}", line);
+            }
+        }
+    }
+}
+
+fn print_usage(s: &str) {
+    println!("{} -r PATTERN [-f FILE]", s);
+}
+
+fn split(s: String) -> Vec<String> {
+    let lines: Vec<&str> = s.split('\n').collect();
+
+    let mut splits: Vec<String> = Vec::with_capacity(lines.len());
+
+    for line in lines {
+        splits.push(String::from(line));
+    }
+
+    splits
+}
+
+fn replace(src: Vec<String>, pattern: String) -> Vec<String> {
+    let mut v: Vec<String> = vec![];
+
+    for line in src {
+        if !line.contains(&pattern) {
+            v.push(line);
+        }
+    }
+
+    v
+}
+
+fn read_file(_path: String) -> Result<String, Box<dyn std::error::Error + 'static>> {
+    let path = std::path::Path::new(&_path);
+
+    if !path.exists() {
+        eprintln!("ERROR 1: {} is not a valid path!", _path);
+    }
+
+    if !path.is_file() {
+        eprintln!("ERROR 2: {} is valid, but not a file!", _path);
+    }
+
+    let file = std::fs::read_to_string(path)?;
+
+    Ok(file)
+}
