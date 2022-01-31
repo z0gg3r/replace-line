32d52d5b534f7768d631e358b37cb30464e4781a
zocker <zocker@10zen.eu>
2022-01-31 07:28:24 +0100

    add print_usage

--- replace-line.sh
+++ replace-line.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
+# SPDX-FileCopyRightText: 2022 zocker <zocker@10zen.eu>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+print_usage()
+{
+	printf "replace-line "
+	printf "\033[32;48mFILE\033[0m "
+	printf "\033[33;48mPATTERN\033[0m "
+	printf "[\033[35;48mREPLACEMENT\033[0m]"
+	printf "\n"
+}
 
 replace_line()
 {
@@ -16,7 +27,11 @@ die()
 	exit 1
 }
 
-if [ -e "$1" ]
+if [ "$#" -lt 2 ]
+then
+	print_usage
+	exit 1
+elif [ -e "$1" ]
 then
 	cat "$1" | while read -r line
 	do
